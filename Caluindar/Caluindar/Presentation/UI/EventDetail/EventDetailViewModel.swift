1e63175ba86886120915e108c5987e7caf25fe4f
takuto277 <takuto.72.soccer@gmail.com>
2024-11-09 12:58:39 +0900

    イベント詳細画面　ボタン押下時の処理追加

--- Caluindar/Caluindar/Presentation/UI/EventDetail/EventDetailViewModel.swift
+++ Caluindar/Caluindar/Presentation/UI/EventDetail/EventDetailViewModel.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 import Combine
 
+enum EventDetailButtonType {
+    case edit
+    case trash
+    case deleteAlert
+}
+
 extension EventDetailViewModel {
     struct Input {
-        
+        let tappedButton: AnyPublisher<EventDetailButtonType, Never>
     }
     
     class Output: ObservableObject {
         @Published var eventData: EventData = EventData(
+            id: UUID(),
+            eventIdentifier: "",
             title: "",
             startDate: Date(),
             endDate: Date(),
             location: "",
             isAllDay: false,
             color: nil)
+        @Published var showAlert = false
+        @Published var dismiss = false
     }
 }
 
@@ -38,6 +48,27 @@ final class EventDetailViewModel: ObservableObject {
     }
     
     func transform(input: Input) -> Output {
+        input.tappedButton
+            .sink { [weak self] buttonType in
+                guard let self else { return }
+                Task {
+                    switch buttonType {
+                    case .edit:
+                        break
+                    case .trash:
+                    Task { @MainActor in
+                        self.output.showAlert = true
+                    }
+                    case .deleteAlert:
+                        try await self.useCase.deleteEvent(eventDate: self.output.eventData)
+                        Task { @MainActor in
+                            self.output.dismiss = true
+                        }
+                    }
+                }
+            }
+            .store(in: &cancellables)
+        
         return output
     }
 }
