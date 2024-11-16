e49ea0883320179ab616a99fb64b957412f281fa
takuto277 <takuto.72.soccer@gmail.com>
2024-11-17 00:23:34 +0900

    fix Calendar of Cell Layout

--- Caluindar/Caluindar/Presentation/UI/CustomCalendarCell.swift
+++ Caluindar/Caluindar/Presentation/UI/CustomCalendarCell.swift
@@ -10,15 +10,15 @@ import FSCalendar
 
 class CustomCalendarCell: FSCalendarCell {
     private var eventLabels: [UILabel] = []
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         let labelHeight: CGFloat = 15
@@ -27,31 +27,36 @@ class CustomCalendarCell: FSCalendarCell {
             label.textColor = UIColor(named: "Basic")
         }
     }
-
-func configure(with events: [String]?) {
-    // 既存のラベルをクリア
-    eventLabels.forEach { $0.removeFromSuperview() }
-    eventLabels.removeAll()
-
-    // 新しいイベントラベルを追加
-    events?.enumerated().forEach { index, event in
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 10)
-        label.textColor = UIColor(named: "Basic")
-        label.numberOfLines = 1
-        label.lineBreakMode = .byTruncatingTail
+    
+    func configure(with events: [EventData]) {
+        // 既存のラベルをクリア
+        eventLabels.forEach { $0.removeFromSuperview() }
+        eventLabels.removeAll()
         
-        if index < 3 {
-            label.text = event
-        } else if index == 3 {
-            label.text = "..."
-        } else {
-            return
+        // 新しいイベントラベルを追加
+        var displayedEvents = events.prefix(3)
+        for event in displayedEvents {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 10)
+            label.textColor = UIColor(named: "Basic")
+            label.numberOfLines = 1
+            label.lineBreakMode = .byTruncatingTail
+            label.text = event.title
+            contentView.addSubview(label)
+            eventLabels.append(label)
         }
         
-        contentView.addSubview(label)
-        eventLabels.append(label)
+        if events.count > 3 {
+            let moreLabel = UILabel()
+            moreLabel.font = UIFont.systemFont(ofSize: 10)
+            moreLabel.textColor = UIColor(named: "Basic")
+            moreLabel.numberOfLines = 1
+            moreLabel.lineBreakMode = .byTruncatingTail
+            moreLabel.text = "..."
+            contentView.addSubview(moreLabel)
+            eventLabels.append(moreLabel)
+        }
+        
+        setNeedsLayout()
     }
-    setNeedsLayout()
-}
 }
