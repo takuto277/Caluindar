1893d86354b1ce8cec77341945826a42f2c1c536
takuto277 <takuto.72.soccer@gmail.com>
2024-11-17 00:20:57 +0900

    add CoreDataStack

---
+++ Caluindar/Caluindar/Data/Model/CoreDataStack.swift
@@ -0,0 +1,38 @@
+//
+//  Untitled.swift
+//  Caluindar
+//
+//  Created by 小野拓人 on 2024/11/10.
+//
+
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CaluindarData")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Unresolved error \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
